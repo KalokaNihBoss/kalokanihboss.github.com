a29163c3ff6c9b4a6e6f425e0d1ca9ca7dc5a70f
kalokanihboss <kalokaradya@gmail.com>
2023-01-08 06:49:11 +0700

    mengedit bagian copyright atau bagian footer menjadi tahun 2023

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+const homeLink = document.querySelector("#homeLink");
+const homeLinkP = document.querySelector("#homeLink p");
 
+homeLink.addEventListener("mouseover", function () {
+    // homeLinkP.classList.toggle("d-none");
+    homeLinkP.classList.toggle("homeLinkAnimation");
+});
